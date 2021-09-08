08d7c23c7f3a6f80b67c1dedeb03f9582e5ae140
mrcheater <mister.cheater@list.ru>
2021-09-08 21:01:52 +0300

    WIP

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,7 @@
-import { DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb'
+import { DynamoDB, ListTablesCommand } from '@aws-sdk/client-dynamodb'
 
 test('wip', async () => {
-  const client = new DynamoDBClient({
+  const client = new DynamoDB({
     region: 'test',
     endpoint: { hostname: 'localhost', port: 8000, path: '/', protocol: 'http' },
     credentials: {
@@ -9,11 +9,79 @@ test('wip', async () => {
       secretAccessKey: 'secretAccessKey',
     },
   })
-  const command = new ListTablesCommand({})
-  try {
-    const results = await client.send(command)
-    console.log(results?.TableNames?.join('\n'))
-  } catch (err) {
-    console.error(err)
+
+  const results = await client.listTables({})
+
+  expect(results?.TableNames).toEqual([])
+
+  const eventsTableName = 'events'
+
+  await client.createTable({
+    TableName: 'events',
+    BillingMode: 'PAY_PER_REQUEST',
+    StreamSpecification: {
+      StreamEnabled: true,
+      StreamViewType: 'NEW_IMAGE',
+    },
+    AttributeDefinitions: [
+      { AttributeName: 'index', AttributeType: 'S' },
+      { AttributeName: 'type', AttributeType: 'S' },
+      { AttributeName: 'payload', AttributeType: 'S' },
+      { AttributeName: 'aggregateId', AttributeType: 'S' },
+      { AttributeName: 'aggregateVersion', AttributeType: 'N' },
+      { AttributeName: 'timestamp', AttributeType: 'N' },
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'index',
+        KeyType: 'HASH',
+      },
+      {
+        AttributeName: 'aggregateVersion',
+        KeyType: 'RANGE',
+      },
+    ],
+  })
+
+  const event = {
+    aggregateId: 'id1',
+    aggregateVersion: 1,
+    type: 'QQQ',
+    payload: {
+      value: 42,
+    },
+    timestamp: Date.now(),
   }
+
+  console.log('putItem start')
+  await client.putItem({
+    TableName: eventsTableName,
+    Item: {
+      index: {
+        S: (new Date(event.timestamp).toISOString() + Math.random()).replace(/[\.:]/g, '-'),
+      },
+      aggregateId: {
+        S: event.aggregateId,
+      },
+      aggregateVersion: {
+        N: `${event.aggregateVersion}`,
+      },
+      type: {
+        S: event.type,
+      },
+      payload: {
+        S: JSON.stringify(event.payload),
+      },
+      timestamp: {
+        N: `${event.timestamp}`,
+      },
+    },
+  })
+  console.log('putItem end')
+
+  console.log(
+    await client.query({
+      TableName: eventsTableName,
+    })
+  )
 })
